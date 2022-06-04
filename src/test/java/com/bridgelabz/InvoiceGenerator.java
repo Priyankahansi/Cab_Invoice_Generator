8dcb8addf772bb31e7a5a72b2950de4557e5b7c5
Priyanka <priyankahansi95@gmail.com>
2022-06-04 19:53:45 +0530

    added:Multiple Rides

--- src/test/java/com/bridgelabz/InvoiceGenerator.java
+++ src/test/java/com/bridgelabz/InvoiceGenerator.java
@@ -5,16 +5,24 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
-        public double calculateFare(double distance, int time) {
-            double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-            if (totalFare < MINIMUM_FARE) {
-                return MINIMUM_FARE;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE) {
+            return MINIMUM_FARE;
 
-            } else {
-                return totalFare;
-            }
+        } else {
+            return totalFare;
         }
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
+}
 
 
 
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -12,6 +12,7 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
     @Test
     public void givenLessDistanceAndTimeShouldReturnMinFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
@@ -21,6 +22,15 @@ public class InvoiceServiceTest {
         Assert.assertEquals(5, fare, 0.0);
     }
 
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
+    }
 }
 
 
